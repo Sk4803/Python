4918b76f7b57e418b0d151bfb52ba5de2d3fbfba
Sk4803 <80695246+Sk4803@users.noreply.github.com>
2022-02-06 22:39:33 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number :"))
+sum1 = 0
+l = []
+temp = num
+order = 0
+while temp > 0 :
+    digit = temp%10
+    l.append(digit)
+    temp = temp//10
+    order += 1
+for i in l :
+    i = i**order
+    sum1 += i
+if num == sum1 :
+    print(num, "is an Armstrong number.")
+else :
+    print(num, "is not an Armstrong number.")
